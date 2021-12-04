2f4ee79fab2e33d0a1b3fc5130d212f02686716d
MerySh <87581472+MerySh@users.noreply.github.com>
2021-12-04 23:08:06 +0400

    Delete settings.hpp

--- settings.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SETTINGS_HPP
-#define SETTINGS_HPP
-
-extern int level; 
-
-void settings(const int row, const int col);
-void print_settings(const int row, const int col, std::string levels[], const int size);
-
-#endif
