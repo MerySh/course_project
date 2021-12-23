170ae062ad1810d6d7b995b29ba2539e97bb213a
MerySh <87581472+MerySh@users.noreply.github.com>
2021-12-23 23:28:55 +0400

    Add files via upload

---
+++ Include/settings.hpp
@@ -0,0 +1,9 @@
+#ifndef SETTINGS_HPP
+#define SETTINGS_HPP
+
+extern int level; 
+
+void settings(const int row, const int col);
+void print_settings(const int row, const int col, std::string levels[], const int size);
+
+#endif
